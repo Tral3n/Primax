bcf2d0521189d005e76e9c7487cac1dccdd3e4d2
Tral3n <tralendk@gmail.com>
2021-09-08 22:04:41 -0500

    conexion heriku

--- Conexion/Conexion.php
+++ Conexion/Conexion.php
@@ -1,8 +1,8 @@
 <?php
-$db_host="localhost";
+$db_host="us-cdbr-east-04.cleardb.com"; //localhost
 $db_nombre="primax";
-$db_usuario="root";
-$db_pw="";
+$db_usuario="be638dfe02bc87";//root
+$db_pw="18c8a290";// ""
 $conexion=mysqli_connect($db_host,$db_usuario,$db_pw,$db_nombre);
 
 
