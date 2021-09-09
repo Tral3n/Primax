bfc3eb14a6c8717f850718e96ec0d30c80d7dab1
Tral3n <tralendk@gmail.com>
2021-09-09 00:03:34 -0500

    /Controladores deleted

--- Registro.php
+++ Registro.php
@@ -33,7 +33,7 @@ session_start();
 
 
 
-                <form action="controladores/controladorRegistro.php" method="post" align="center">
+                <form action="controladorRegistro.php" method="post" align="center">
                    
 
                 <div>
