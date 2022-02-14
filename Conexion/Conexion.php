4c09522e2830dfebc3b7f0c4420cf611c7e2f7c6
Tral3n <tralendk@gmail.com>
2022-02-13 23:42:23 -0500

    ppi 2022

--- Conexion/Conexion.php
+++ Conexion/Conexion.php
@@ -1,10 +1,11 @@
 <?php
-$db_host="localhost";
-$db_nombre="primax";
-$db_usuario="root";
-$db_pw="";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$db_host="us-cdbr-east-04.cleardb.com"; //localhost
+$db_nombre="heroku_f7036baf347b6a5"; //primax
+$db_usuario="be638dfe02bc87";//root
+$db_pw="18c8a290";// ""
+$db = substr($url["path"], 1);
 $conexion=mysqli_connect($db_host,$db_usuario,$db_pw,$db_nombre);
-
-
 
 ?>
